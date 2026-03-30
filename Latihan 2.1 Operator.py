90bd3880c8a999ee3ca585d910ab66f4285f0e80
Divia Arinda <diviaarindasurya@gmail.com>
2026-03-30 09:22:16 +0700

    Add files via upload

---
+++ Latihan 2.1 Operator.py
@@ -0,0 +1,11 @@
+# Latihan 2.1 Operator
+# Program menghitung rata-rata 3 bilangan
+
+# Input dari user
+a = float(input("Masukkan nilai pertama: "))
+b = float(input("Masukkan nilai kedua: "))
+c = float(input("Masukkan nilai ketiga: "))
+
+rata = (a + b + c) / 3
+
+print("Rata-rata =", rata)
